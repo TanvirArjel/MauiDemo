2e32e1c441d00a2e2459e776df6791c8bc2beddc
Eilon Lipton <Eilon@users.noreply.github.com>
2021-08-19 11:15:55 -0700

    Update demo app to .NET 6 preview7/rc1

--- MauiBlazorApp/MauiBlazorApp/Startup.cs
+++ MauiBlazorApp/MauiBlazorApp/Startup.cs
@@ -16,7 +16,7 @@ namespace MauiBlazorApp
         public void Configure(IAppHostBuilder appBuilder)
         {
             appBuilder
-                .RegisterBlazorMauiWebView(typeof(Startup).Assembly)
+                .RegisterBlazorMauiWebView()
                 .UseMicrosoftExtensionsServiceProviderFactory()
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
